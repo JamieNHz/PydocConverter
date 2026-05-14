ca2fa22ffb6c918f4d5a8bfb3f6e0c549997893b
Jamie Brown <jamboffc@googlemail.com>
2026-05-14 22:16:06 +0100

    feat: importing argeaparse and specifying output and input arguments

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import aspose.words as aw
+import argparse
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description="Convert ODT document to pdf"
+    )
+
+    parser.add_argument("input_file")
+    parser.add_argument("output_file")
+
+    args = parser.parse_args()
+
+    print(args.input_file)
+    print(args.output_file)
 
 
 if __name__ == "__main__":
-    main
+    main()
