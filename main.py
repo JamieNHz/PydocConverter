d739289db3bbdc9bf148fb26d830018bb640f02c
Jamie Brown <jamboffc@googlemail.com>
2026-05-14 22:44:23 +0100

    feat: converted args to "--input" and "--output" and passing these into aspose module to save to a pdf format

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def main():
         description="Convert ODT document to pdf"
     )
 
-    parser.add_argument("input_file")
-    parser.add_argument("output_file")
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required = True)
 
     args = parser.parse_args()
 
-    print(args.input_file)
-    print(args.output_file)
+    inputfile = args.input
+    outputfile = args.output
+
+    doc = aw.Document(inputfile)
+    doc.save(outputfile)
 
 
 if __name__ == "__main__":
